91fcfc8882b65f96a1b44fb13050b2032e8eac6b
John P. Feltz <jfeltz@gmail.com>
2015-05-09 19:47:53 -0500

    removing checkPath

--- src/Pipe/Conf.hs
+++ src/Pipe/Conf.hs
@@ -8,8 +8,8 @@ import           Data.String.Util
 import           Distribution.InstalledPackageInfo as DIP
 import qualified Filesystem.Path.CurrentOS as P
 import           Package.Conf
-import           Pipe
 import qualified System.Directory as D
+import           FileSystem       
        
 confError :: FilePath -> String -> M r
 confError path fatal = 
@@ -39,14 +39,6 @@ fromParseResults conf (ParseOk cabalWarnings fields)
         (P.decodeString (head $ haddockInterfaces fields))
         (P.decodeString $ head (haddockHTMLs fields) ++ "/")
         (DIP.exposed fields)
-
-checkPath :: Bool -> P.FilePath -> String -> IO (Maybe String) 
-checkPath dir path name = do 
-  exists <- predicate . P.encodeString $ path 
-  return $ if exists then Nothing else Just $ "missing: " ++ name ++ ' ':msg'
-  where 
-    (predicate, msg') =
-      if dir then (D.doesDirectoryExist, "dir") else (D.doesFileExist, "file")
 
 diagnosePaths :: Conf -> M [String]
 diagnosePaths conf = do
