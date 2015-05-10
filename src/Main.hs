7b649adbb09c17d426751ac80a4d30fb9d6b9845
John P. Feltz <jfeltz@gmail.com>
2015-05-09 19:49:25 -0500

    version for next release and import correction

--- src/Main.hs
+++ src/Main.hs
@@ -13,20 +13,22 @@ import           System.Environment
 
 main :: IO ()
 main = do 
-  -- Check for help mode arg first. There doesn't seem to be a good way to do this
-  -- otherwise with opt-parse applicative.
+  -- Check for help mode arg first. There doesn't seem to be a good way to do 
+  -- this otherwise with opt-parse applicative.
   args <- getArgs
   case L.partition (== "help") args of 
     ([], args') -> do
       options <- handleParseResult $ execParserPure (prefs idm) parserInfo args'
       
-      -- Run the package processing pipeline. Packages that can't be completed due
-      -- to either conversion error or user error, should, if necessary, leave a safe partially
-      -- completed state on the FS that can be handled by dependant tools, e.g. Emacs helm-dash.
+      -- Run the package processing pipeline. Packages that can't be
+      -- completed due -- to either conversion error or user error, should, if
+      -- necessary, leave a safe partially -- completed state on the FS that
+      -- can be handled by dependant tools, e.g. Emacs helm-dash.
 
       runM (newEnv (not . quiet $ options)) . runEffect $
-       cons_writeFiles (P.decodeString $ outputDir options) -- writes converted html, haddock, and sql db
-       <-< pipe_Conf                         -- yields vetted package configurations
+      -- writes converted html, haddock, and sql db
+       cons_writeFiles (P.decodeString $ outputDir options) 
+       <-< pipe_Conf                         -- yields vetted package configs
        <-< pipe_ConfFp (dbprovider options)  -- yields GHC package config files
        <-< prod_Dependencies options         -- yields packages from options
     (_, rest) -> toHelp docs rest
@@ -36,4 +38,4 @@ main = do
    parserInfo = info (helper <*> parser)  $
      header "dash-haskell v1.1.0.0, a dash docset construction tool for Haskell packages"
      <> progDesc "additional help is available with \"dash-haskell help <topic|option>\""
-     <> footer "http://www.github.com/jfeltz/dash-haskell (C) John P. Feltz 2014"
+     <> footer "http://www.github.com/jfeltz/dash-haskell (C) John P. Feltz 2014, 2015"
