d01fa76bf6fe01a7dc376046c06f7b805b0c205a
John P. Feltz <jfeltz@gmail.com>
2015-05-16 14:46:24 -0500

    drafting

--- src/Main.hs
+++ src/Main.hs
@@ -1,41 +1,70 @@
 import qualified Filesystem.Path.CurrentOS as P
 import           Control.Monad.M
-import           Pipe
-import           Pipe.FileSystem
-import           Pipe.Conf
-import           Pipe.Db
+import           Control.Monad.Reader
+import           Pipes
+-- import           Pipe.FileSystem
+-- import           Pipe.Conf
+-- import           Pipe.Db
 import           Options.Applicative
 import           Options.Documentation
-import           Options
+import qualified Options as O
 
 import qualified Data.List as L
 import           System.Environment
+import           Db
+import           Data.Maybe
+                 
+cabalSandboxConfig :: FilePath  
+cabalSandboxConfig = "./cabal.sandbox.config" 
+
+defaultStack :: [Db]
+defaultStack = [Global, User]
+             
+-- | The default stack defined when cabal is sourced for packages
+cabalStack :: (Db, [Db])
+cabalStack = (Sandbox cabalSandboxConfig, defaultStack)
+           
+toMaybe :: Bool -> a -> Maybe a
+toMaybe True a  = Just a 
+toMaybe False _ = Nothing 
+    
+dbs :: O.Options -> [Db]
+dbs opts = 
+  catMaybes $
+    sandbox : 
+    map (uncurry toMaybe) [(O.global opts, Global), (O.user opts, User)]
+  where 
+    sandbox :: Maybe Db 
+    sandbox = Sandbox . fromMaybe cabalSandboxConfig <$> O.sandbox opts
 
 main :: IO ()
-main = do 
-  -- Check for help mode arg first. There doesn't seem to be a good way to do 
-  -- this otherwise with opt-parse applicative.
-  args <- getArgs
-  case L.partition (== "help") args of 
-    ([], args') -> do
-      options <- handleParseResult $ execParserPure (prefs idm) parserInfo args'
-      
-      -- Run the package processing pipeline. Packages that can't be
-      -- completed due -- to either conversion error or user error, should, if
-      -- necessary, leave a safe partially -- completed state on the FS that
-      -- can be handled by dependant tools, e.g. Emacs helm-dash.
+main = undefined 
 
-      runM (newEnv (not . quiet $ options)) . runEffect $
-      -- writes converted html, haddock, and sql db
-       cons_writeFiles (P.decodeString $ outputDir options) 
-       <-< pipe_Conf                         -- yields vetted package configs
-       <-< pipe_ConfFp (dbprovider options)  -- yields GHC package config files
-       <-< prod_Dependencies options         -- yields packages from options
-    (_, rest) -> toHelp docs rest
+-- main :: IO ()
+-- main = do 
+--   -- Check for help mode arg first. There doesn't seem to be a good way to do 
+--   -- this otherwise with opt-parse applicative.
+--   args <- getArgs
+--   case L.partition (== "help") args of 
+--     ([], args') -> do
+--       options <- handleParseResult $ execParserPure (prefs idm) parserInfo args'
+      
+--       -- Run the package processing pipeline. Packages that can't be
+--       -- completed due -- to either conversion error or user error, should, if
+--       -- necessary, leave a safe partially -- completed state on the FS that
+--       -- can be handled by dependant tools, e.g. Emacs helm-dash.
+
+--       runM (newEnv (not . quiet $ options)) . runEffect $
+--         -- writes converted html, haddock, and sql db
+--         cons_writeFiles (P.decodeString $ outputDir options) 
+--         <-< pipe_Conf                         -- yields vetted package configs
+--         <-< pipe_ConfFp (dbprovider options)  -- yields GHC package config files
+--         <-< prod_Dependencies options         -- yields packages from options
+--     (_, rest) -> toHelp docs rest
   
-  where 
-   parserInfo :: ParserInfo Options
-   parserInfo = info (helper <*> parser)  $
-     header "dash-haskell v1.1.0.0, a dash docset construction tool for Haskell packages"
-     <> progDesc "additional help is available with \"dash-haskell help <topic|option>\""
-     <> footer "http://www.github.com/jfeltz/dash-haskell (C) John P. Feltz 2014, 2015"
+--   where 
+--    parserInfo :: ParserInfo Options
+--    parserInfo = info (helper <*> parser)  $
+--      header "dash-haskell v1.1.0.0, a dash docset construction tool for Haskell packages"
+--      <> progDesc "additional help is available with \"dash-haskell help <topic|option>\""
+--      <> footer "http://www.github.com/jfeltz/dash-haskell (C) John P. Feltz 2014, 2015"
