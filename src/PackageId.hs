e22dcb7fe0e86f355b5860e68019c05952a893d8
John P. Feltz <jfeltz@gmail.com>
2015-05-16 16:16:58 -0500

    reduction (2)

--- src/PackageId.hs
+++ src/PackageId.hs
@@ -1,12 +1,12 @@
 module PackageId where
-import qualified Distribution.Package as C
+import Distribution.Package
 import Distribution.Version
 
 emptyVersion :: Version
 emptyVersion = Version [] []
               
-unversioned :: C.PackageId -> C.PackageId
-unversioned p = p { C.pkgVersion = emptyVersion } 
+unversioned :: PackageId -> PackageId
+unversioned p = p { pkgVersion = emptyVersion } 
 
-versionless :: String -> C.PackageId
-versionless n = C.PackageIdentifier (C.PackageName n) emptyVersion 
+versionless :: String -> PackageId
+versionless n = PackageIdentifier (PackageName n) emptyVersion 
