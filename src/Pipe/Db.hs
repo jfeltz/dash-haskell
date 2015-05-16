a9f7a8e8451469393da5f5a79f99bb7a9eefaf16
John P. Feltz <jfeltz@gmail.com>
2015-05-16 18:47:29 -0500

    update

--- src/Pipe/Db.hs
+++ src/Pipe/Db.hs
@@ -11,11 +11,23 @@ import qualified Data.List as L
 import qualified Data.Set as S
 import qualified Data.Text as T
 import qualified Distribution.Package as C
-import           Filesystem
+import           Filesystem hiding (readFile)
 import qualified Filesystem.Path.CurrentOS as P
 import           Db
 -- imports necessary for working with Cabal package db tools 
 import Distribution.Package
+-- import           Control.Monad.IO.Class
+import           FilePath
+import           Text.ParserCombinators.Parsec hiding (State)
+import qualified Distribution.Simple.Compiler        as CC
+import qualified Distribution.Simple.GHC             as CG
+import qualified Distribution.Simple.PackageIndex    as CI
+import qualified Distribution.Simple.Program.Builtin as CP
+import qualified Distribution.Simple.Program.Db      as CP
+import qualified Distribution.Simple.Program         as CP
+import qualified Distribution.Verbosity           as CVB
+import qualified Distribution.Version             as CV
+import qualified Distribution.Text                as CT
 
 type DbStack = [Db]
 
@@ -86,3 +98,63 @@ pipe_ConfFp stack = do
           mapped = uncurry fromPair . (\(db, members) -> (db, map snd members))
         in 
           mapM_ yield =<< lift (L.concat <$> mapM mapped pairings)
+
+field :: String -> Parser String
+field str =
+  string str 
+  >> char ':'
+  >> many (char ' ') 
+  >> manyTill anyToken (void (char '\n') <|> eof)
+
+singleField :: String -> Parser String 
+singleField str = try (field str) <|> (anyToken >> singleField str) 
+ 
+parsedDbPath :: String -> M String 
+parsedDbPath path = do
+  buf <- liftIO $ readFile path
+  case runParser (singleField "package-db") () path buf of
+    Left  str -> err . show $ str
+    Right db      -> return db 
+
+cabalDb :: Db -> M CC.PackageDB
+cabalDb (Sandbox config) = do
+  check_result <- liftIO $ checkFile config "cabal sandbox config"
+  case check_result of
+    Nothing      -> CC.SpecificPackageDB <$> parsedDbPath config 
+    Just err_str -> err err_str 
+cabalDb (Global   ) = return CC.GlobalPackageDB
+cabalDb (User     ) = return CC.UserPackageDB
+cabalDb (Path    p) = return . CC.SpecificPackageDB $ p
+        
+-- | This will warn here or downstream when using out of bounds haddock 
+-- ghc-pkg: >=7.10 && <7.10.2
+ghcVersionRange :: CV.VersionRange
+ghcVersionRange = 
+ CV.intersectVersionRanges 
+   (CV.orLaterVersion (CV.Version [7,10]   [])) 
+   (CV.earlierVersion (CV.Version [7,10,2] [])) 
+
+toIndex :: [CC.PackageDB] -> M CI.InstalledPackageIndex 
+toIndex stack = do 
+  version <- liftIO $ CP.programFindVersion CP.ghcPkgProgram CVB.normal "ghc-pkg"
+  case version of
+    Nothing ->
+      warning $ "unable to determine ghc-pkg version, \n" ++ clause
+    Just v ->
+      unless (not $ CV.withinRange v ghcVersionRange) $ 
+        warning $
+        "ghc-pkg version: " ++ show version ++ "not within allowable range, \n" 
+        ++ clause
+  
+  liftIO $ CG.getInstalledPackages CVB.normal stack program_conf
+  where
+    program_conf :: CP.ProgramConfiguration
+    program_conf = CP.restoreProgramDb [CP.ghcPkgProgram] CP.emptyProgramDb
+    clause :: String
+    clause = 
+      "results may not match current supported haddock: " 
+      ++ show (CT.disp ghcVersionRange) 
+
+-- TODO 
+-- apply lookupDependency :: PackageInstalled a => PackageIndex a -> Dependency -> [(Version, [a])] 
+-- at this point
