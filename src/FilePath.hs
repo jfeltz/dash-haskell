edc724d2abb694157f47c18b36beec1d8550fa68
John P. Feltz <jfeltz@gmail.com>
2015-05-16 18:33:44 -0500

    reduction

--- src/FilePath.hs
+++ src/FilePath.hs
@@ -1,13 +1,16 @@
 module FilePath where
-
-import qualified Filesystem.Path.CurrentOS as P
 import qualified System.Directory as D
 
-checkPath :: Bool -> P.FilePath -> String -> IO (Maybe String) 
+checkPath :: Bool -> FilePath -> String -> IO (Maybe String) 
 checkPath dir path name = do 
-  exists <- predicate . P.encodeString $ path 
+  exists <- predicate path 
   return $ if exists then Nothing else Just $ "missing: " ++ name ++ ' ':msg'
   where 
     (predicate, msg') =
       if dir then (D.doesDirectoryExist, "dir") else (D.doesFileExist, "file")
 
+checkDir :: FilePath -> String -> IO (Maybe String)
+checkDir = checkPath True  
+
+checkFile :: FilePath -> String -> IO (Maybe String)
+checkFile = checkPath False  
