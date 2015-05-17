7f57106c67c5b77401a8d46cbce2a06c9912aa10
John P. Feltz <jfeltz@gmail.com>
2015-05-16 23:57:39 -0500

    update

--- src/Package/Conf.hs
+++ src/Package/Conf.hs
@@ -1,10 +1,9 @@
 module Package.Conf where
-import qualified Filesystem.Path.CurrentOS as P
 import Distribution.Package (PackageIdentifier)
 
 data Conf = Conf
   { pkg           :: PackageIdentifier
-  , interfaceFile :: P.FilePath -- interface, i.e. .haddock file
-  , htmlDir       :: P.FilePath -- root html source directory
+  , interfaceFile :: FilePath -- interface, i.e. .haddock file
+  , htmlDir       :: FilePath -- root html source directory
   , exposed       :: Bool -- module exposure flag
   }
